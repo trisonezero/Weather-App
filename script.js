4104f3b587910d95482ee2a04c0f0e42de8905fe
Tristan <tris10@my.yorku.ca>
2024-09-19 18:09:34 -0400

    First

---
+++ script.js
@@ -0,0 +1,37 @@
+openWeatherApiKey = '1e2cb3c6f90875e3709b54ae9422e33b';
+
+const searchBtn = document.getElementById("searchBtn").addEventListener('click', function(){
+    getLoc(document.getElementById('loc').value);
+})
+
+
+function getLoc(loc) {
+    const geoFetch = `http://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=1&appid=${openWeatherApiKey}`
+    fetch(geoFetch)
+        .then(geoResponse => geoResponse.json())
+        .then(geoData => {
+            lat = geoData[0].lat;
+            lon = geoData[0].lon;
+
+            const weaFetch = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}&units=metric`
+            fetch(weaFetch)
+            .then(weaResponse => weaResponse.json())
+            .then(weaData => {
+                d = Math.round(weaData["main"].temp) + "\u2103\t" + weaData["weather"][0].description;
+                document.getElementById("basicWeather").innerText = d;
+                document.getElementById("descIco").src = `https://openweathermap.org/img/wn/${weaData["weather"][0].icon}@2x.png`
+                if (String(weaData["weather"][0].id).charAt(0) == '7'){
+                    weatherType = "Atmosphere"
+                }
+                else{
+                    
+                    weatherType = weaData["weather"][0].main
+                }
+                document.body.style.backgroundImage = `url('pictures/${weatherType}.jpg')`;
+            })
+        })
+    
+    .catch(err => alert("Invalid City"))
+
+    
+}
